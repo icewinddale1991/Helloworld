4b8358cf0073bf2664e8fea16d2cf16fae617a2f
icewinddale1991 <lplagiatl@gmial.com>
2024-12-07 10:11:02 +0300

    Added more exclamation sings

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,59 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Helloworld!");
-        System.out.println("привет мир!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+    }
+
+    public static void task1() {
+        System.out.println("Задача1");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+    }
+
+    public static void task2() {
+        System.out.println("Задача2");
+        var dog = 8.0;
+        dog = dog + 2;
+        var cat = 3.6;
+        cat = cat + 2;
+        var paper = 763789;
+        paper = paper + 2;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+    }
+
+    public static void task3() {
+        System.out.println("Задача3");
+        var dog = 8.0;
+        dog = dog - 3.5;
+        var cat = 3.6;
+        cat = cat - 1.6;
+        var paper = 763789;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper+"\n");
+    }
+    public static void task4() {
+        System.out.println("Задача4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend * 19;
+        System.out.println(friend);
+        friend = friend / 2;
+        System.out.println(friend);
+    }
+    public static void task5() {
+
     }
 }
+
