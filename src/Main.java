f3d3855a88550b20fd25a94406dd2ad8df847346
icewinddale1991 <lplagiatl@gmial.com>
2024-12-11 11:49:07 +0300

    Corrections variables Pt.2

--- src/Main.java
+++ src/Main.java
@@ -70,7 +70,7 @@ public class Main {
 
     public static void task5() {
         System.out.println("Задача 5");
-        byte potalCansOfPaint =120;
+        byte TpotalCansOfPaint =120;
         byte whitePaintOnTheRoom =2;
         byte brownPaintOnTheRoom =4;
         int totalCansForTheRoom = (whitePaintOnTheRoom + brownPaintOnTheRoom);
