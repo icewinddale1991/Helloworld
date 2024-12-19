f4b28a39b3edc7fed2e0c6f7083b349b05b2d022
icewinddale1991 <lplagiatl@gmial.com>
2024-12-19 18:22:59 +0300

    Cycles progress

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,55 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Helloworld!");
+        taskOne();
+        taskTwo();
+        taskThree();
+        taskFour();
+        taskFive();
+
+
+    }
+
+    public static void taskOne() {
+        System.out.println("Задача 1");
+        for (int i = 1; i < 10; i = i + 1) {
+            System.out.println( "Итерация цикла " +i);
+        }
+        System.out.println();
+    }
+
+    public static void taskTwo() {
+        System.out.println("Задача 2");{
+            for (int i = 10; i >0; i = i - 1) {
+            System.out.println("Итерация цикла " +i);
+            }
+        }
+        System.out.println();
+    }
+
+    public static void taskThree() {
+        System.out.println("Задача 3");
+        for (int i = 0; i < 17; i = i + 2) {
+            System.out.println("Итерация цикла " + i);
+        }
+
+        System.out.println();
+    }
+
+    public static void taskFour() {
+        System.out.println("Задача 4");
+        for (int i = 10; i >=-10; i = i - 1) {
+            System.out.println("Итерация цикла " + i);
+        }
+
+        System.out.println();
+    }
+
+    public static void taskFive() {
+        System.out.println("Задача 5");
+
+        System.out.println();
+
     }
 }
+
